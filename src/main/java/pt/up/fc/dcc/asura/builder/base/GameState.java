460cfc44ff18b9155220ddfb089c705dddbcd3bb
josepaiva94@gmail.com <josepaiva94@gmail.com>
2018-07-23 17:43:02 +0100

    Throwing exceptions from game state

--- src/main/java/pt/up/fc/dcc/asura/builder/base/GameState.java
+++ src/main/java/pt/up/fc/dcc/asura/builder/base/GameState.java
@@ -1,5 +1,7 @@
 package pt.up.fc.dcc.asura.builder.base;
 
+import pt.up.fc.dcc.asura.builder.base.exceptions.BuilderException;
+import pt.up.fc.dcc.asura.builder.base.exceptions.PlayerException;
 import pt.up.fc.dcc.asura.builder.base.messaging.PlayerAction;
 import pt.up.fc.dcc.asura.builder.base.messaging.StateUpdate;
 import pt.up.fc.dcc.asura.builder.base.movie.GameMovieBuilder;
@@ -11,11 +13,14 @@ import java.util.Map;
  * through the methods.
  * <p>
  * <dl>
- * <dt><code>prepare(GameMovieBuilder,Set&lt;String&gt;)</code></dt>
+ * <dt><code>prepare(GameMovieBuilder,Map&lt;String,String&gt;)</code></dt>
  * <dd>to initialize the state</dd>
  * <p>
- * <dt><code>execute(String,GameUpdate)</code></dt>
+ * <dt><code>execute(GameMovieBuilder,String,PlayerAction)</code></dt>
  * <dd>to execute player's command to change the state</dd>
+ * <p>
+ * <dt><code>StateUpdate getStateUpdateFor(String)</code></dt>
+ * <dd>get an object to update player state</dd>
  * <p>
  * <dt><code>endRound(GameMovieBuilder)</code></dt>
  * <dd>get new frame at the end of a round</dd>
@@ -36,35 +41,41 @@ public interface GameState {
     /**
      * Prepare state before the game starts
      *
-     * @param movieBuilder Game movie builder
-     * @param players      Player names indexed by ID
+     * @param movieBuilder {@link GameMovieBuilder} Game movie builder
+     * @param players      {@link Map} Player names indexed by ID
+     * @throws BuilderException - If an exception occurs related to the game manager, game state, or in the builder framework
+     * @throws PlayerException  - If an exception occurs related to a player behavior
      */
-    void prepare(GameMovieBuilder movieBuilder, Map<String, String> players);
+    void prepare(GameMovieBuilder movieBuilder, Map<String, String> players) throws BuilderException, PlayerException;
 
     /**
      * Changes game state according to update issued by a concrete player
      *
-     * @param movieBuilder {@link GameMovieBuilder}
-     *                     Game movie builder
-     * @param playerId     Player identifier
+     * @param movieBuilder {@link GameMovieBuilder} Game movie builder
+     * @param playerId     {@link String} Player identifier
      * @param action       {@link PlayerAction} Player action
+     * @throws BuilderException - If an exception occurs related to the game manager, game state, or in the builder framework
+     * @throws PlayerException  - If an exception occurs related to a player behavior
      */
-    void execute(GameMovieBuilder movieBuilder, String playerId, PlayerAction action);
+    void execute(GameMovieBuilder movieBuilder, String playerId, PlayerAction action) throws BuilderException, PlayerException;
 
     /**
      * Get an object to update player state
      *
-     * @param player Player to update
-     * @return state update to send to player
+     * @param player {@link String} Player to update
+     * @return {@link StateUpdate} state update to send to player
+     * @throws BuilderException - If an exception occurs related to the game manager, game state, or in the builder framework
      */
-    StateUpdate getStateUpdateFor(String player);
+    StateUpdate getStateUpdateFor(String player) throws BuilderException;
 
     /**
      * Called when all players' commands where executed in a round
      *
-     * @param movieBuilder Game movie builder
+     * @param movieBuilder {@link GameMovieBuilder} Game movie builder
+     * @throws BuilderException - If an exception occurs related to the game manager, game state, or in the builder framework
+     * @throws PlayerException  - If an exception occurs related to a player behavior
      */
-    void endRound(GameMovieBuilder movieBuilder);
+    void endRound(GameMovieBuilder movieBuilder) throws BuilderException, PlayerException;
 
     /**
      * Is the game still running?
@@ -76,7 +87,8 @@ public interface GameState {
     /**
      * Finalize the state. Run any necessary tasks when the game has ended.
      *
-     * @param movieBuilder Game movie builder
+     * @param movieBuilder {@link GameMovieBuilder} Game movie builder
+     * @throws BuilderException - If an exception occurs related to the game manager, game state, or in the builder framework
      */
-    void finalize(GameMovieBuilder movieBuilder);
+    void finalize(GameMovieBuilder movieBuilder) throws BuilderException;
 }
